38acc9b65d61cd7e6a1ce79de0579ec4ea645a3a
aapanasenkov <babunbox@gmail.com>
2022-09-23 00:41:47 +0300

    fix

--- H/W_6/Task1/index.js
+++ H/W_6/Task1/index.js
@@ -1,45 +1,14 @@
-let users = [
-    {
-     "isActive": true,
-     "balance": "226.60",
-     "name": "Eugenia Sawyer",
-     "phone": "+1 (840) 583-3207",
-    },
-    {
-     "isActive": true,
-     "balance": "2613.77",
-     "name": "Pauline Gallegos",
-     "phone": "+1 (985) 593-3328",
-    },
-    {
-     "isActive": false,
-     "balance": "3976.41",
-     "name": "Middleton Chaney",
-     "phone": "+1 (995) 591-2478",
-     },
-    {
-     "isActive": false,
-     "balance": "1934.58",
-     "name": "Burns Poole",
-     "phone": "+1 (885) 559-3422",
-     },
-    {
-     "isActive": true,
-     "balance": "3261.65",
-     "name": "Mcfadden Horne",
-     "gender": "male",
-     "phone": "+1 (942) 565-3988",
-    },
-]
-
-function displayAllPhoneActiveUsers (users) {
-    let phones = [];
-    users.forEach(element => {
-        if (element.isActive && element.balance > 2000) {
-            phones.push(element.phone);
+function displayLastNumberMoreThan100() {
+    let lastPrmt;
+    for (let i = 0; i < 10; i++) {
+        lastPrmt = prompt('Введите число больше 100');
+        if (lastPrmt < 100) {
+            continue;
+        } else {
+            console.log(lastPrmt);
+            return;
         }
-    });
-    console.log(phones);
+    }
 }
 
-displayAllPhoneActiveUsers(users);
+displayLastNumberMoreThan100();
